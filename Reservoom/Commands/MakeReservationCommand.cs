54ce52ed2c65cb0ed2e312b48eb0180f45fdda3a
KaiXinFang <1842904290@qq.com>
2022-10-16 22:59:36 +0800

    mvvm

---
+++ Reservoom/Commands/MakeReservationCommand.cs
@@ -0,0 +1,69 @@
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using Reservoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reservoom.Commands
+{
+    public class MakeReservationCommand : CommandBase
+    {
+        private readonly MakeReservationViewModel _makeReservationViewModel;
+        private readonly Hotel _hotel;
+
+        public MakeReservationCommand(MakeReservationViewModel makeReservationViewModel, Hotel hotel)
+        {
+            _makeReservationViewModel = makeReservationViewModel;
+            _hotel = hotel;
+
+            _makeReservationViewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+        /*
+         * 判断此命令是否能执行，返回true则能执行，自动执行Excute函数
+         * 返回false则不能执行，就不会自动执行Excute函数
+         */
+        public override bool CanExecute(object parameter)
+        {
+            return !string.IsNullOrEmpty(_makeReservationViewModel.Username) &&
+                _makeReservationViewModel.FloorNumber > 0 &&
+                base.CanExecute(parameter);
+        }
+
+        //定义调用此命令时，自动执行的方法
+        public override void Execute(object parameter)
+        {
+            Reservation reservation = new Reservation(
+                new RoomID(_makeReservationViewModel.FloorNumber, _makeReservationViewModel.RoomNumber),
+                _makeReservationViewModel.Username,
+                _makeReservationViewModel.StartDate,
+                _makeReservationViewModel.EndDate);
+
+            try
+            {
+                _hotel.MakeReservation(reservation);
+
+                MessageBox.Show("Successfully reserved room.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (ReservationConflictException)
+            {
+                MessageBox.Show("This room is already taken.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MakeReservationViewModel.Username) ||
+                e.PropertyName == nameof(MakeReservationViewModel.FloorNumber))
+            {
+                OnCanExecutedChanged();
+            }
+        }
+    }
+}
