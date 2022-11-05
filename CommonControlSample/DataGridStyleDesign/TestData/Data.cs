6db2f1109267e17eda80dea692cb57a25bc991b9
KaiXinFang <1842904290@qq.com>
2022-11-05 09:33:52 +0800

    DataGridStyleDesign

---
+++ CommonControlSample/DataGridStyleDesign/TestData/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridStyleDesign.TestData
+{
+    public class Data
+    {
+        public Data()
+        {
+            listData = new List<object>();
+            for (int i = 0; i < 100; i++)
+            {
+                listData.Add(new { Name = "老王" + i, Age = 12, Sex = "男", Time = DateTime.Now });
+            }
+        }
+        public List<object> listData { get; set; }
+
+    }
+}
