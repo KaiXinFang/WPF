cb98ee41350194b3087c95f34990ed5c59fbb2c0
KaiXinFang <1842904290@qq.com>
2022-11-06 21:11:54 +0800

    TreeView的功能实现

---
+++ CommonControlSample/TreeViewDesign/HeaderToImageConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeViewDesign
+{
+    /// <summary>
+    /// 转换器:将针对盘符，文件夹，文件的目录路径转换为对应的图片
+    ///     界面文件传输过来是一个目录路径
+    ///     要针对不同的路径，返回一个图标资源，才能赋值给Image类的Source属性
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //获取路径(value是界面文件传输过来的path值)
+            var path = (string)value;
+            //如果路径为空则忽略
+            if (path == null)
+            {
+                return null;
+            }
+            //获取文件或者文件夹的名字
+            var name = MainWindow.GetFileFolderName(path);
+            
+            //默认图标为文件图标
+            var imagePath = "Images/file.png";
+            //如果name为空，则默认为磁盘图标
+            if (string.IsNullOrEmpty(name))
+            {
+                imagePath = "Images/driver.png";
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                imagePath = "Images/file-close.png";
+            }
+            //否则返回一个图片
+            return new BitmapImage(new Uri($"pack://application:,,,/{imagePath}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommonControlSample/TreeViewDesign/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TreeViewDesign
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region MainWindow
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region on Loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //GetLogicalDrives检查电脑逻辑驱动器(内存盘)的名称
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //创建新的item
+                var item = new TreeViewItem()
+                {
+                    //设置头节点名称和路径
+                    Header = drive,
+                    Tag = drive//相当于为当前节点设置标签名称并指定跳转路径
+                };
+
+                //可理解成添加子节点
+                item.Items.Add(null);
+
+                //监听树节点扩展(当 IsExpanded 属性从 false 更改为 true 时发生)
+                item.Expanded += Folder_Expand;
+                //添加到树结构中
+                FolderView.Items.Add(item);
+            }
+        }
+        #endregion
+
+        //点击节点进行展开时，响应展开事件
+        #region Folder_Expand
+        private void Folder_Expand(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            //如果当前节点有子节点，说明之前打开过这个节点，则不再进行遍历查找的操作
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+            }
+            //清空当前节点
+            item.Items.Clear();
+            //获取整体路径
+            var fullPath = (string)item.Tag;
+            #region get Folders
+            //创建一个存放目录的空列表
+            var directories = new List<string>();
+            //尝试从改路径中获取文件夹
+            try
+            {
+                //从当前路径获取文件夹
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch
+            {
+
+            }
+            //遍历每一个文件夹路径
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(directoryPath),
+                    Tag = directoryPath//把文件夹路径赋值给Tag属性
+                };
+                //添加子项以触发扩展事件（文件夹有子项可以Expand,但是文件本身就不能在Expand了）
+                subItem.Items.Add(null);
+                subItem.Expanded += Folder_Expand;//相当于递归
+                //将子项加入父级item(就是事件触发者)
+                item.Items.Add(subItem);
+            });
+            #endregion
+
+            #region get Files
+            //创建一个存放目录的空列表
+            var files = new List<string>();
+            //尝试从该路径中获取文件
+            try
+            {
+                //从当前路径获取文件
+                var fs = Directory.GetFiles(fullPath);
+                if (fs.Length > 0)
+                {
+                    files.AddRange(fs);
+                }
+            }
+            catch
+            {
+
+            }
+            //遍历每一个文件
+            files.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(filePath),
+                    Tag = filePath//把文件路径赋值给Tag属性
+                };
+                //文件本身就不能在Expand,所以不再挂接Expanded事件(这是和遍历文件夹的唯一区别)
+                //将子项加入父级item(就是事件触发者)
+                item.Items.Add(subItem);
+            });
+            #endregion
+        }
+        #endregion
+
+        #region GetFileFolderName
+        public static string GetFileFolderName(string directoryPath)
+        {
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                return string.Empty;
+            }
+            //统一路径中的 / 号，便于查找
+            var normallizedPath = directoryPath.Replace('/', '\\');
+            //寻找最后一个 \ 出现的索引
+            var lastIndex = normallizedPath.LastIndexOf('\\');
+            if (lastIndex <= 0)
+            {
+                return directoryPath;
+            }
+            //返回文件名或者文件夹名
+            return directoryPath.Substring(lastIndex + 1);
+        }
+    }
+    #endregion 
+
+}
